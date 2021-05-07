9ca8512815e22f7b42f5e9f67037701467c7f3d8
BlueSkeptical <xg2543@utexas.edu>
2021-05-07 14:00:55 -0500

    add locust

---
+++ locustfile.py
@@ -0,0 +1,19 @@
+from locust import HttpUser, task, between
+
+class my_locust(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/")
+
+    @task
+    def make_predictions(self):
+        self.client.post("/predict", 
+        json={"CHAS":{"0":0},
+        "RM":{"0":6.575},
+        "TAX":{"0":296.0},
+        "PTRATIO":{"0":15.3},
+        "B":{"0":396.9},
+        "LSTAT":{"0":4.98}})
+
